4475fb7ca18c3560e24e3ef31ce7c9b8a040f466
Kazuya Takei <myself@attakei.net>
2021-11-03 01:18:23 +0900

    First plugin source with bundler

---
+++ src/plugin.js
@@ -0,0 +1,28 @@
+const createFinishSection = () => {
+  const section = document.createElement("section");
+  section.innerHTML = "Shutdown!";
+  return section;
+};
+
+const shutdownPresentation = (deck) => {
+  console.debug("Time is over!! Shutdown presentation");
+  const section = createFinishSection();
+  document.getElementsByClassName("slides")[0].append(section);
+  while (!deck.isLastSlide()) {
+    deck.next();
+  }
+};
+
+const Plugin = () => {
+  return {
+    id: "timelimit",
+    init: (deck) => {
+      deck.on("ready", () => {
+        console.debug("Start timer of revealjs-timelimit");
+        setTimeout(() => shutdownPresentation(deck), 300 * 1000);
+       });
+    }
+  }
+}
+
+export default Plugin;
