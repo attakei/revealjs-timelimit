c7494aaf02a17b4893a8933a92dfcef67f1ca0e1
Kazuya Takei <myself@attakei.net>
2021-11-04 12:20:00 +0900

    Configure for adding license information to bundled files

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,19 @@
+const path = require('path');
+const license = require("rollup-plugin-license");
+
+const licenseOptions = {
+  banner: {
+    commentStyle: "ignored",
+    content: `
+      <%= pkg.name %> v<%= pkg.version %>
+      <%= pkg.homepage %>
+      <%= pkg.license %>
+
+      (c) 2021 Kazuya Takei
+    `,
+  }
+}
+
 export default {
   input: "./src/plugin.js",
   output: [
@@ -5,10 +21,16 @@ export default {
       file: "./dist/timelimit.js",
       format: "iife",
       name: "RevealTimelimit",
+      plugins: [
+        license(licenseOptions)
+      ],
     },
     {
       file: "./dist/timelimit.esm.js",
       format: "esm",
+      plugins: [
+        license(licenseOptions)
+      ],
     },
   ],
 };
